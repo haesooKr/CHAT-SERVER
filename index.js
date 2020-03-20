fdfbd47ed1f2f3f4d5e8074b000b428db71b0f41
haesooKr <gotn676@gmail.com>
2020-03-19 21:20:18 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const expres = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen()
