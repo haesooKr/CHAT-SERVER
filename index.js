57a37b6f73f8566f9903b272c845082e76f9690f
haesooKr <gotn676@gmail.com>
2020-03-20 16:11:46 -0400

    Add join

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-  console.log(`We have a new connection!!! (${socket})`);
+  console.log(`We have a new connection!!!`);
+
+  socket.on('join', ({name, room}) => {
+    console.log(name, room);
+  })
 
   socket.on('disconnect', () => {
-    console.log(`${socket} user had left`)
+    console.log(`User had left`)
   })
 })
 
