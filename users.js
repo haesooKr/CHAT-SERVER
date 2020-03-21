aaf1e476e3b8ec33a539bdb86440617f6ea394f7
haesooKr <gotn676@gmail.com>
2020-03-21 02:40:47 -0400

    Fixed Syntax Error line 21

--- users.js
+++ users.js
@@ -18,7 +18,7 @@ const addUser = ({ id, name, room }) => {
 }
 
 const removeUser = (id) => {
-  const index = users.find((user) => user.id === id);
+  const index = users.findIndex((user) => user.id === id);
 
   if(index !== -1){
     return users.splice(index, 1)[0];
